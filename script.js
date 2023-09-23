e285e3ba95e13a9eca86097f3d55d9c97b24fc7a
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-09-23 18:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let buttons = document.getElementsByClassName("btn");
+let stopButton = document.getElementsByClassName("stop");
+
+for (let i=0; i<buttons.length; i++){
+	buttons[i].addEventListener("click", (event) => {
+		
+	});
+}
